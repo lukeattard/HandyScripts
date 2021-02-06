04f53250f9981f7900d56203093b56020ddf88b0
Luke Attard <luke@rapiddata.com.au>
2021-02-07 03:17:35 +1100

    Update opkgUpgradeAll.sh

--- opkgUpgradeAll.sh
+++ opkgUpgradeAll.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/ash
 opkg update
-opkg list-upgradable | awk 'BEGIN {system("sleep 10")} {system("opkg upgrade "$1)}
+#sleep 10 is used to ensure that the lock from opkg list-upgrade is released before we attempt to install the first upgrade, most likely can be less then 10, but better safe then having errors at times.
+opkg list-upgradable | awk 'BEGIN {system("sleep 10")} {system("opkg upgrade "$1)}'
 echo "Please check for errors above, and enjoy"
