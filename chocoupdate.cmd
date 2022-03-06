524316b6553f2ebc0ebe16cccb9b99052ea1c7de
Luke Attard <luke@rapiddata.com.au>
2022-03-06 22:07:19 +1100

    Cleaned up the output and updated the cmd to upgrading as per choc instructions

--- chocoupdate.cmd
+++ chocoupdate.cmd
@@ -1,4 +1,9 @@
 @echo off
-choco feature enable -n allowGlobalConfirmation
-choco outdated | for /f "delims=| tokens=1 skip=1" %%i in ('findstr "|"') DO choco upgrade %%i
+choco feature enable -n allowGlobalConfirmation 1> nul
+choco outdated | for /f "delims=| tokens=1 skip=1" %%i in ('findstr "|"') DO @(
+  @echo Upgrading %%i
+  @Echo:
+  @Echo:
+  choco upgrade %%i
+)
 refreshenv
