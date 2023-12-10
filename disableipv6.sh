9d0c262c78d3879b1b9058424811e8b5a7a5815f
Luke Attard <luke@rapiddata.com.au>
2023-12-10 13:33:04 +1100

    modified disable ipv6 script

--- disableipv6.sh
+++ disableipv6.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sysctl -a 2>/dev/null | grep ipv6 | awk '/disable/{system(sysctl =1)}'
+echo $#
+if [ $# -gt 0 ]; then
+    if [ "$1" == "-p" ]; then
+        sysctl -a 2>/dev/null | grep ipv6 | awk '/disable/{system("sysctl "$1"=1")}' >> /etc/sysctl.conf
+    fi
+else
+    sysctl -a 2>/dev/null | grep ipv6 | awk '/disable/{system("sysctl "$1"=1")}'
+fi
 
